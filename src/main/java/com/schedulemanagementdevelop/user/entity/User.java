23ce5907e82cf66b0b008b6f053d51f672eb7beb
soomin0209 <soomin02094@gmail.com>
2026-01-09 00:51:56 +0900

    chore: 3 Layer Architecture를 기반으로 user 패키지 구성

---
+++ src/main/java/com/schedulemanagementdevelop/user/entity/User.java
@@ -0,0 +1,24 @@
+package com.schedulemanagementdevelop.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/schedulemanagementdevelop/user/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.schedulemanagementdevelop.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository {
+}
