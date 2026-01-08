a25e9ebf1854798abbd3f1d55a157f20979188ff
soomin0209 <soomin02094@gmail.com>
2026-01-08 17:35:05 +0900

    init

---
+++ src/main/java/com/schedulemanagementdevelop/ScheduleManagementDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.schedulemanagementdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManagementDevelopApplication.class, args);
+    }
+
+}
