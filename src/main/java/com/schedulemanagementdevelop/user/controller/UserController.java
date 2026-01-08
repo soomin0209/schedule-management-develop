0534943a547997405482d628452fc42b51ff0000
soomin0209 <soomin02094@gmail.com>
2026-01-09 01:22:05 +0900

    feat: Lv 2. 유저 CRUD - Read(전체 조회) 구현

--- src/main/java/com/schedulemanagementdevelop/user/controller/UserController.java
+++ src/main/java/com/schedulemanagementdevelop/user/controller/UserController.java
@@ -2,13 +2,17 @@ package com.schedulemanagementdevelop.user.controller;
 
 import com.schedulemanagementdevelop.user.dto.CreateUserRequest;
 import com.schedulemanagementdevelop.user.dto.CreateUserResponse;
+import com.schedulemanagementdevelop.user.dto.GetUserResponse;
 import com.schedulemanagementdevelop.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +25,10 @@ public class UserController {
             @RequestBody CreateUserRequest request
     ) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(request));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<GetUserResponse>> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
     }
 }
--- src/main/java/com/schedulemanagementdevelop/user/service/UserService.java
+++ src/main/java/com/schedulemanagementdevelop/user/service/UserService.java
@@ -2,11 +2,14 @@ package com.schedulemanagementdevelop.user.service;
 
 import com.schedulemanagementdevelop.user.dto.CreateUserRequest;
 import com.schedulemanagementdevelop.user.dto.CreateUserResponse;
+import com.schedulemanagementdevelop.user.dto.GetUserResponse;
 import com.schedulemanagementdevelop.user.entity.User;
 import com.schedulemanagementdevelop.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +28,18 @@ public class UserService {
                 savedUser.getCreatedAt(),
                 savedUser.getModifiedAt()
         );
+    }
+
+    @Transactional(readOnly = true)
+    public List<GetUserResponse> findAll() {
+        List<User> users = userRepository.findAll();
+        return users.stream()
+                .map(user -> new GetUserResponse(
+                        user.getId(),
+                        user.getName(),
+                        user.getEmail(),
+                        user.getCreatedAt(),
+                        user.getModifiedAt()
+                )).toList();
     }
 }
