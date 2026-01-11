867f745221f8acb833b7e024535a8b5ab07de857
soomin0209 <soomin02094@gmail.com>
2026-01-11 14:22:26 +0900

    refactor: 불필요한 메서드 제거

--- src/main/java/com/schedulemanagementdevelop/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/schedulemanagementdevelop/schedule/repository/ScheduleRepository.java
@@ -1,7 +1,6 @@
 package com.schedulemanagementdevelop.schedule.repository;
 
 import com.schedulemanagementdevelop.schedule.entity.Schedule;
-import com.schedulemanagementdevelop.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +10,4 @@ import java.util.List;
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     @Query("SELECT s FROM Schedule s WHERE :userId IS NULL OR s.user.id = :userId")
     List<Schedule> findByUser(@Param("userId") Long userId);
-
-    Long user(User user);
 }
