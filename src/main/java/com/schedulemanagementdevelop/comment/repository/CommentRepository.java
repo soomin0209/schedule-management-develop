6a7ef4cb37a8acf9846070c53fdad23ba8eb1669
soomin0209 <soomin02094@gmail.com>
2026-01-12 14:10:24 +0900

    refactor: 사용하지 않는 메서드, import 제거

--- src/main/java/com/schedulemanagementdevelop/comment/repository/CommentRepository.java
+++ src/main/java/com/schedulemanagementdevelop/comment/repository/CommentRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByScheduleId(Long scheduleId);
-
     List<Comment> findByScheduleIdOrderByCreatedAt(Long scheduleId);
 }
--- src/main/java/com/schedulemanagementdevelop/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/schedulemanagementdevelop/schedule/repository/ScheduleRepository.java
@@ -3,7 +3,6 @@ package com.schedulemanagementdevelop.schedule.repository;
 import com.schedulemanagementdevelop.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
